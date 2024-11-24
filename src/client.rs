b00fc2a3f7b9dc1b273641c1493d82748075a51a
zjp <jiping_zhou@foxmail.com>
2024-11-24 14:27:11 +0800

    feat(github): only use GH_TOKEN to construct Client

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,9 @@ use std::sync::LazyLock;
 
 /// A request builder for github apis.
 pub fn github() -> Builder {
-    static CLIENT: LazyLock<Client> = LazyLock::new(Client::new_from_env);
+    static CLIENT: LazyLock<Client> = LazyLock::new(|| {
+        let token = std::env::var("GH_TOKEN").ok();
+        Client::new(token.as_deref())
+    });
     CLIENT.get()
 }
