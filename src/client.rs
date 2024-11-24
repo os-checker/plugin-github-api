301ec9de50fe687d84dd471d218a3ec416932ef0
zjp <jiping_zhou@foxmail.com>
2024-11-24 14:30:08 +0800

    feat(github): use GITHUB_TOKEN as a fallback for Client

--- src/client.rs
+++ src/client.rs
@@ -4,7 +4,9 @@ use std::sync::LazyLock;
 /// A request builder for github apis.
 pub fn github() -> Builder {
     static CLIENT: LazyLock<Client> = LazyLock::new(|| {
-        let token = std::env::var("GH_TOKEN").ok();
+        let token = std::env::var("GH_TOKEN")
+            .or_else(|_| std::env::var("GITHUB_TOKEN"))
+            .ok();
         Client::new(token.as_deref())
     });
     CLIENT.get()
